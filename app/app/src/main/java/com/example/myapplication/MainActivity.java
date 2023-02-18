b43654892ca0d2c354f4d528ffdb1e706e6b80dc
Helkaph <arkat.taiyo@gmail.com>
2023-02-18 12:24:22 +0300

    Initial commit

---
+++ app/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    protected void onStart() {
+        super.onStart();
+    }
+    protected void onResume() {
+        super.onResume();
+    }
+    protected void onStop() {
+        super.onStop();
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
